70be058f9ad654df99fca2c82d00d0351c9d3576
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-03-26 16:24:53 -0600

    Prueba

---
+++ src/count_nucleotides.py
@@ -0,0 +1,6 @@
+'''
+Encabezado
+'''
+dna=input('Introduzca la secuencia de DNA cuyo tipo y numero de nucleotidos desee determinar:\n')
+
+print(f"Frecuencia de nucleotidos \n A:{dna.count('A')} C:{dna.count('C')}")
