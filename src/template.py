0cd860b8164f82bbda22b2d1a97eda28cb1e310f
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-03-28 18:54:09 -0600

    se crea archivo template para programas de python

---
+++ src/template.py
@@ -0,0 +1,22 @@
+''' NAME
+      
+
+VERSION
+        1.0
+
+AUTHOR
+        Victor Ulises Plascencia Perez
+
+DESCRIPTION
+       
+
+USAGE
+         
+
+ARGUMENTS
+
+SEE ALSO
+
+GitHub link
+       
+'''
