f0dbf1fbd0588ee2a01a5828891de1db766937dc
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-03-29 18:51:05 -0600

    se genera archivo para contar nt

--- src/count_nucleotides.py
+++ src/count_nucleotides.py
@@ -1,6 +1,26 @@
-'''
-Encabezado
-'''
-dna=input('Introduzca la secuencia de DNA cuyo tipo y numero de nucleotidos desee determinar:\n')
+''' NAME
+        Count_nucleotides
 
-print(f"Frecuencia de nucleotidos \n A:{dna.count('A')} C:{dna.count('C')}")
+VERSION
+        1.0
+
+AUTHOR
+        Victor Ulises Plascencia Perez
+
+DESCRIPTION
+        Contador de nucleotidos en una secuencia dada
+
+USAGE
+        ramdom motif [OPTIONS] 
+
+ARGUMENTS
+
+SEE ALSO
+       
+'''
+
+dna='AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+
+print(f'La cantidad de nucleotidos que contiene la secuencia es:\n{len(dna)}')
+
+print(f"A = {dna.count('A')}\tT = {dna.count('T')}\tG = {dna.count('G')}\tC = {dna.count('C')}")
