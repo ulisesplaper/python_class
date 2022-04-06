8c2d849b09ac97cd58bd1b988894e7856af454d3
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-04-06 18:20:06 -0500

    Codificar el programa y anadir link gitHub

--- src/eliminate_adapters.py
+++ src/eliminate_adapters.py
@@ -1,8 +1,6 @@
-''' 
-NAME
-    eliminate_adapters
-      
-
+''' NAME
+        eliminate_adapters
+    
 VERSION
         1.0
 
@@ -23,15 +21,34 @@ SEE ALSO
     none
 
 GitHub link
+    https://github.com/ulisesplaper/python_class/blob/master/src/eliminate_adapters.py
        
 '''
+
 # Abrir el archivo, guardar cada linea en una lista y cerrar el archivo
+
+file = open("docs/4_input_adapters.txt")
+seqs_adapters = file.readlines()
+file.close()
+
 
 # Crear un archivo destino para almacenar las secuencias sin adaptadores
 
-# Recorrer la lista y eliminar los primeros 14 elementos de cada string.
+file = open('results/sequences_woadpters.txt', 'w')
+
+
+# Recorrer la lista y eliminar los primeros 14 elementos de cada string
 # Agregar cada secuencia al archivo destino
+
+for seq_adapter in seqs_adapters:
+    file.write(f"{seq_adapter[14:-1]}\n")
+
 
 # Cerrar el archivo destino
 
+file.close()
+
+
 # Imprimir informacion del archivo para el usuario
+
+print('Archivo generado: /results/sequences_woadpters.txt')
