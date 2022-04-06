ed7e549b1a506d7957547d736149ebb3e44dfbcd
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-04-06 18:03:22 -0500

    Anadir encabezado y comentarios de logica del programa

---
+++ src/eliminate_adapters.py
@@ -0,0 +1,37 @@
+''' 
+NAME
+    eliminate_adapters
+      
+
+VERSION
+        1.0
+
+AUTHOR
+        Victor Ulises Plascencia Perez
+
+DESCRIPTION
+    Elimina los primeros 14 nucleotidos (adapatadores) de secuencias de DNA
+       
+
+USAGE
+    py eliminate_adapters.py
+         
+ARGUMENTS
+    none
+    
+SEE ALSO
+    none
+
+GitHub link
+       
+'''
+# Abrir el archivo, guardar cada linea en una lista y cerrar el archivo
+
+# Crear un archivo destino para almacenar las secuencias sin adaptadores
+
+# Recorrer la lista y eliminar los primeros 14 elementos de cada string.
+# Agregar cada secuencia al archivo destino
+
+# Cerrar el archivo destino
+
+# Imprimir informacion del archivo para el usuario
