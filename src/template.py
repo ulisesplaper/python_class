d51c0fffa6492c01b8fe8c5caaaa398439177203
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-04-06 20:46:43 -0500

    Actualizar template de encabezado de programas

--- src/template.py
+++ src/template.py
@@ -1,22 +1,17 @@
 ''' NAME
-      
-
+        program_name
 VERSION
         1.0
-
 AUTHOR
         Victor Ulises Plascencia Perez
-
 DESCRIPTION
-       
-
+    
 USAGE
-         
-
+    
 ARGUMENTS
-
+    none
 SEE ALSO
-
+    none
 GitHub link
-       
+    https://github.com/ulisesplaper/python_class/blob/master/src/program_name.py     
 '''
