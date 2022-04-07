9292b6f2afa70ecc5fc889485b74f298155cb275
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-04-06 19:45:27 -0500

    Anadir el encabezado y comentarios de la logica del programa

---
+++ src/fasta_converter.py
@@ -0,0 +1,38 @@
+''' NAME
+        fasta_converter
+    
+VERSION
+        1.0
+
+AUTHOR
+        Victor Ulises Plascencia Perez
+
+DESCRIPTION
+    Convierte un archivo tabular de secuencias en un archivo fasta
+       
+
+USAGE
+    py fasta_converter.py
+         
+ARGUMENTS
+    none
+    
+SEE ALSO
+    none
+
+GitHub link
+    https://github.com/ulisesplaper/python_class/blob/master/src/fasta_converter.py
+       
+'''
+
+# Abrir el archivo, guardar cada linea en una lista y cerrar el archivo
+
+# Crear un archivo vacio fasta
+
+# Recorrer cada uno de los elementos de la lista
+# Anadir el caracter ">" y cambiar el tabulador por salto de linea
+# Escribir cada secuencia en el archivo de destino
+
+# Cerrar el archivo fasta
+
+# Imprimir informacion para el usuario
