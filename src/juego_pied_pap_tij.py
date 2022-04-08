8ded4cfa8f1c67f89b3eed3f057adb47d2226932
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-04-07 22:19:08 -0500

    Anadir encabezado y comentarios de logica del programa

---
+++ src/juego_pied_pap_tij.py
@@ -0,0 +1,46 @@
+''' juego_pied_pap_tij
+        program_name
+VERSION
+        1.0
+AUTHOR
+        Victor Ulises Plascencia Perez
+DESCRIPTION
+    Jugar el juego "piedra o papel o tijera" contra la computadora.
+USAGE
+    py juego_pied_pap_tij.py
+ARGUMENTS
+    none
+SEE ALSO
+    none
+GitHub link
+    https://github.com/ulisesplaper/python_class/blob/master/src/juego_pied_pap_tij.py     
+'''
+
+
+# Definir las opciones validas
+
+# Solicitar al usuario su eleccion
+
+# Calcular la eleccion de la computadora.
+
+# Comparar la eleccion de la computadora con la eleccion del usuario
+# Si ambas elecciones son iguales
+# imprimir "empate"
+
+# Si la eleccion del usuario es "piedra"
+# Si la eleccion de la computadora es "tijera"
+# Imprimir "usuario gana"
+# Si la eleccion no es "tijera"
+# Imprimir "computadora gana"
+
+# Si la eleccion del usuario es "tijera"
+# Si la eleccion de la computadora es "papel"
+# Imprimir "usuario gana"
+# Si la eleccion no es "papel"
+# Imprimir "computadora gana"
+
+# Si la eleccion del usuario es "papel"
+# Si la eleccion de la computadora es "piedra"
+# Imprimir "usuario gana"
+# Si la eleccion no es "piedra"
+# Imprimir "computadora gana"
