71659676a4c58420d349f5a29ae2d6c7f1b3c6c9
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-04-20 22:13:43 -0500

    Corregir comentarios y encabezado

--- src/juego_pied_pap_tij.py
+++ src/juego_pied_pap_tij.py
@@ -1,5 +1,5 @@
-''' juego_pied_pap_tij
-        program_name
+''' Name
+    piedra_papel_tijera.py
 VERSION
         1.0
 AUTHOR
@@ -19,7 +19,7 @@ GitHub link
 
 # Definir las opciones validas
 
-# Solicitar al usuario su eleccion
+# Solicitar al usuario una eleccion valida
 
 # Calcular la eleccion de la computadora.
 
@@ -30,17 +30,17 @@ GitHub link
 # Si la eleccion del usuario es "piedra"
 # Si la eleccion de la computadora es "tijera"
 # Imprimir "usuario gana"
-# Si la eleccion no es "tijera"
+# Si la eleccion de la computadora no es "tijera"
 # Imprimir "computadora gana"
 
 # Si la eleccion del usuario es "tijera"
 # Si la eleccion de la computadora es "papel"
 # Imprimir "usuario gana"
-# Si la eleccion no es "papel"
+# Si la eleccion de la computadora no es "papel"
 # Imprimir "computadora gana"
 
 # Si la eleccion del usuario es "papel"
 # Si la eleccion de la computadora es "piedra"
 # Imprimir "usuario gana"
-# Si la eleccion no es "piedra"
+# Si la eleccion de la computadora no es "piedra"
 # Imprimir "computadora gana"
