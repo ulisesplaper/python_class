21fb4f7fb2e863d7c82a079bc3c57beedceb784b
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-05-03 16:50:12 -0500

    Agregar manejo de excepciones

--- src/count_nucleotides+input.py
+++ src/count_nucleotides+input.py
@@ -24,18 +24,27 @@ GitHub link
        https://github.com/ulisesplaper/python_class/blob/master/src/count_nucleotides%2Binput.py
 '''
 
-## Solicitar al usuario la secuencia de DNA
+# Solicitar al usuario la secuencia de DNA
 print("Introduzca la secuencia de DNA")
 dna = input().upper()
 
-# Calcular e imprimir la longitud de la secuencia.
-print(f'La cantidad de nucleotidos que contiene la secuencia es:\n{len(dna)}')
+try:
+    # Verificar que la secuencia proporcionada sea AGCT
+    for base in dna:
+        if base not in ["A", "G", "C", "T"]:
+            raise ValueError(
+                f"La secuencia contiene caracteres que no son bases")
+    # Calcular e imprimir la longitud de la secuencia.
+    print(
+        f'La cantidad de nucleotidos que contiene la secuencia es:\n{len(dna)}')
 
-# Contar los nucleotidos
-total_a = dna.count('A')
-total_t = dna.count('T')
-total_g = dna.count('G')
-total_c = dna.count('C')
+    # Contar los nucleotidos
+    total_a = dna.count('A')
+    total_t = dna.count('T')
+    total_g = dna.count('G')
+    total_c = dna.count('C')
 
-# Imprimir resultados
-print(f"A = {total_a}\tT = {total_t}\tG = {total_g}\tC = {total_c}")
+    # Imprimir resultados
+    print(f"A = {total_a}\tT = {total_t}\tG = {total_g}\tC = {total_c}")
+except ValueError:
+    print("\nLa secuencia contiene caracteres que no son bases\n")
