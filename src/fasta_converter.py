5882f2f1914a6d3fd29c9c0f3269da905f7e1ea8
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-05-03 21:05:15 -0500

    Agregar manejo de excepciones

--- src/fasta_converter.py
+++ src/fasta_converter.py
@@ -16,31 +16,48 @@ GitHub link
     https://github.com/ulisesplaper/python_class/blob/master/src/fasta_converter.py     
 '''
 
-# Abrir el archivo, guardar cada linea en una lista y cerrar el archivo
-
-file = open("../data/dna_sequences.txt")
-sequences = file.readlines()
-file.close()
+# Declarar una excepcion para documentos vacios
 
 
-# Crear un archivo vacio fasta
-
-file = open("../results/dna_sequences.fasta", "w")
-
-
-# Recorrer cada uno de los elementos de la lista
-# Eliminar caracteres que no son secuencia
-# Anadir el caracter ">" y cambiar el tabulador por salto de linea
-# Escribir cada secuencia en el archivo de destino
-
-for sequence in sequences:
-    id = '>' + sequence.split("\t")[0] + "\n" + sequence.split("\t")[1]
-    file.write(id.replace('-', '').upper())
+class EmptyDocError(Exception):
+    pass
 
 
-# Cerrar el archivo fasta
-file.close()
+# Intentar realizar las siguientes acciones
+try:
 
+    # Abrir el archivo, guardar cada linea en una lista y cerrar el archivo
 
-# Imprimir informacion para el usuario
-print('Archivo generado: /results/dna_sequences.fasta')
+    file = open("../data/dna_sequences.txt")
+    sequences = file.readlines()
+    file.close()
+
+    # Determinar si la lista esta vacia y regresar el error si lo esta
+    if len(sequences) == 0:
+        raise EmptyDocError("\n El archivo input esta vacio\n")
+
+    # Crear un archivo vacio fasta
+
+    file = open("../results/dna_sequences.fasta", "w")
+
+    # Recorrer cada uno de los elementos de la lista
+    # Eliminar caracteres que no son secuencia
+    # Anadir el caracter ">" y cambiar el tabulador por salto de linea
+    # Escribir cada secuencia en el archivo de destino
+
+    for sequence in sequences:
+        id = '>' + sequence.split("\t")[0] + "\n" + sequence.split("\t")[1]
+        file.write(id.replace('-', '').upper())
+
+    # Cerrar el archivo fasta
+    file.close()
+
+    # Imprimir informacion para el usuario
+    print('Archivo generado: /results/dna_sequences.fasta')
+
+# Indicar al usuario cuando no se encuentre el path del archivo
+except IOError:
+    print("\nEl archivo y/o ruta son incorrectos o no existen\n")
+# Indicar al usuario cuando el archivo este vacio
+except EmptyDocError as ex:
+    print(ex.args[0])
