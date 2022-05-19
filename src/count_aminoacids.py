c4e62ff1ffe8fcba53cd03942d4d1f3878471069
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-05-18 20:03:10 -0500

    Agregar comentarios de logica del programa y encabezado

---
+++ src/count_aminoacids.py
@@ -0,0 +1,32 @@
+''' NAME
+      count_aminoacids.py
+
+VERSION
+        1.0
+
+AUTHOR
+        Victor Ulises Plascencia Perez
+
+DESCRIPTION
+       Calcula el porcentaje de un aminoacido dado en una secuencia proteica
+
+USAGE
+
+        count_aminoacids.py
+         
+ARGUMENTS
+        
+SEE ALSO
+
+GitHub link
+        https://github.com/ulisesplaper/python_class/blob/master/src/count_aminoacids.py
+       
+'''
+# Definir la funcion que calcula el porcentaje de aminoacido
+# Indicar los parametros que recibe la funcion
+# Contar las ocurrencias del aminoacido en la proteina
+# Contar el numero de aminoacidos de la proteina.
+# Calcular el porcentaje del aminoacido y regresar el valor
+
+
+# Evaluar que la funcion regrese valores esperados
