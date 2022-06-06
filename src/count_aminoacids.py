9a1f1a79382b4c3d219567f5ca6240e2ea1277c0
Ulisesplapler <100378012+Ulisesplapler@users.noreply.github.com>
2022-06-05 21:19:05 -0500

    Incluir modulos locales y funcion tripsina_treatment

--- src/count_aminoacids.py
+++ src/count_aminoacids.py
@@ -2,21 +2,22 @@
       count_aminoacids.py
 
 VERSION
-        2.0
+        3.0
 
 AUTHOR
         Victor Ulises Plascencia Perez
 
 DESCRIPTION
        Calcula el porcentaje de un aminoacido dado en una secuencia proteica
+       obtenida de un archivo fasta
 
 USAGE
 
-        count_aminoacids.py [-h] [-a AMINOACID_LIST] protein
+        count_aminoacids.py [-h] [-a AMINOACID_LIST] path
 
 ARGUMENTS
         positional arguments:
-            protein               protein sequence
+            path                  path of the fasta protein file
 
         options:
           -h, --help            show this help message and exit
@@ -30,7 +31,9 @@ GitHub link
 '''
 # Importar librerias necesarias
 import argparse
-import string
+import protein_tools as pt
+import re
+import fasta_tools as ft
 
 
 # Declarar las excepciones para aminoacido y proteina incorrecta
@@ -52,63 +55,47 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-a', '--aminoacid_list',
                     type=list, help='aminoacid list without comas, \
 spaces, or special characters', required=False, default='ailmfwyv')
-parser.add_argument("protein", help='protein sequence')
+parser.add_argument(
+    "path", help='path of the fasta protein file', metavar='path', type=str)
 
 # Leer los argumentos de la terminal
 args = parser.parse_args()
 
-# Definir la funcion que calcula el porcentaje de aminoacido
-# Indicar los parametros que recibe la funcion
-
-
-def get_aa_percentage(peptide_sequence, aminoacid_list):
-    '''
-    Returns the porcentaje of aminoacids list in a protein sequence 
-        Parameters
-            peptide_sequence (str): a protein sequence
-            aminoacid_list (str): aminoacid list
-        Returns:
-            get_aa_percentage(float): porcentaje of a amionacids list of protein sequence
-
-
-    '''
-    peptide_sequence = peptide_sequence.upper()
-
-    # Verificar que la secuencia proteica ingresada sea correcta
-    # Regresar el error correspondiente cuando no sea correcta
-    aminoacids = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K',
-                  'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
-    for block in peptide_sequence:
-        if block not in aminoacids:
-            raise NotCorrectProteinError(
-                'NotCorrectProteinError <A correct protein sequence\
- was not entered>')
-    # Inicializar una variable para llevar la cuenta de las ocurrencias
-    # de los aminoacidos de la lista
-    # Recorrer la lista de aminoacidos
-    # contar las ocurrencia en la secuencia proteica y almacenar el resultado
-    # Si algun caracter no corresponde con un aminoacido, regresar el error.
-    peptide_count = 0
-    for aminoacid in aminoacid_list:
-        aminoacid = aminoacid.upper()
-        if aminoacid not in aminoacids:
-            raise NotAminoacidFoundError(
-                'NotAminoacidFoundError <A correct aminoacid was not entered>')
-        peptide_count += peptide_sequence.count(aminoacid)
-
-     # Calcular el porcentaje de la lista de aminoacidos y regresar el valor
-    peptide_percent = round(peptide_count / len(peptide_sequence) * 100, 3)
-    return peptide_percent
-
 
 # Intentar realizar las acciones
 try:
+    # Obter la secuencia del archivo fasta
+    peptide_sequence = ft.get_sequence(args.path)
+    peptide_sequence = peptide_sequence.upper()
 
-    # Informar al usuario el porcentaje de aminoacido en la secuencia proteica
-    # que ingreso o, en su caso, la lista default
+    # Verificar que la secuencia proteica ingresada sea correcta
+    # Regresar el error correspondiente cuando no sea
+    if re.search("[^ACDEFGHIKLMNPQRSTVWY]", peptide_sequence):
+        raise NotCorrectProteinError(
+            'NotCorrectProteinError <A correct protein sequence\
+ was not entered>')
+
+    # Convertir la lista de aminoacidos a string (para emplear regexp)
+    # Si algun caracter no corresponde con un aminoacido, regresar el error.
+
+    aminoacid_list = "".join(args.aminoacid_list)
+    aminoacid_list = aminoacid_list.upper()
+    if re.search("[^ACDEFGHIKLMNPQRSTVWY]", aminoacid_list):
+        raise NotAminoacidFoundError(
+            'NotAminoacidFoundError <A correct aminoacid was not entered>')
+
+    # Calcular e informar al usuario el porcentaje de aminoacido
+    # en la secuencia proteica que ingreso o, en su caso, la lista default
     print(
         f"El porcentaje de {args.aminoacid_list} en la secuencia dada es:\
-\n {get_aa_percentage(args.protein, args.aminoacid_list)}")
+\n{pt.get_aa_percentage(peptide_sequence, aminoacid_list)}")
+
+ # Informarle al usuario el numero de peptidos generados
+ # tras el tratamiento con tripsina
+    print(
+        f"El tratamiento con tripsina producira\
+ {pt.tripsin_treatment(peptide_sequence)} peptidos\n")
+
 
 # Informar si la letra ingresada no corresponde con un aminoacido
 except NotAminoacidFoundError as ex:
@@ -117,3 +104,7 @@ except NotAminoacidFoundError as ex:
 # Informar si la secuencia ingresada contiene letras que no son aminoacidos
 except NotCorrectProteinError as ex:
     print('\n la secuencia proteica ingresada es incorrecta\n' + ex.args[0])
+
+# Informar si el path del archivo es incorrecto
+except FileNotFoundError:
+    print('\n El nombre o ruta del archivo son incorrectos\n')
