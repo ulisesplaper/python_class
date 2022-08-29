f135c3437829b2e5ba4ac6e1cfbd46a0a1332f9e
Ulises Plascencia <ulisesp@lcg.unam.mx>
2022-08-28 23:30:38 -0500

    Aniadir encabezado y logica del programa

---
+++ src/at_rich_seqs_finder.py
@@ -0,0 +1,45 @@
+'''
+ NAME
+      at_rich_seqs_finder.py
+VERSION
+        0.1.0
+AUTHOR
+        Victor Ulises Plascencia Perez
+DESCRIPTION
+       Encuentra regiones ricas en AT de tamanio especificado en un archivo fasta
+USAGE
+        usage: at_rich_seqs_finder.py [-h] -s size [--version] path
+         
+ARGUMENTS
+        positional arguments:
+         path                  path of the nucleotide sequence fasta file
+
+        options:
+         -h, --help            show this help message and exit
+         -s size, --size size  size of minimal region rich in AT
+         --version             show program's version number and exit
+SEE ALSO
+GitHub link
+        https://github.com/ulisesplaper/python_class/blob/master/src/at_rich_seqs_finder.py
+
+'''
+# Determinar argumentos opcionales y posicionales del programa
+
+# leer argumentos desde la terminal y asignarlos a variables
+
+# Extraer la secuencia de nts del archivo fasta
+
+# Crear funcion para validar la secuencia del archivo fasta
+
+# Validar la secuencia con la funcion creada
+# Si la secuencia tiene bases ambiguas, informar su posicion y
+# su identidad
+
+# Si no contiene bases ambiguas, encontrar las secuencias que 
+# contengan AT en la minima cantidad indicada por el usuario y 
+# asignarlas a un iterable
+
+## Recorrer el iterable y determinar el contenido de cada 
+## uno de sus elementos (regiones ricas en AT) y su posicion en 
+# la secuencia original.
+
